e0b7a6e1440220a4f20ad5ca0b7c43d1c009deeb
Edward Lim <sgedwardlim@gmail.com>
2018-03-15 11:26:22 -0700

    Added interface for clock hand

---
+++ ClockHand.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+public class ClockHand implements MoveableShape {
+    public ClockHand(Color color, double thickness, double length) {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+
+    }
+
+    @Override
+    public void translate(int dx, int dy) {
+
+    }
+
+    /**
+     * Moves the clock hand 1/60th of a circle in the clockwise direction
+     */
+    public void tick() {
+
+    }
+}
